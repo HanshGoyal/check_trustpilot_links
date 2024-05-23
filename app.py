2c9c73a24b10b692346aa7057b3bfec316ccd358
Hansh Goyal <99343982+HanshGoyal@users.noreply.github.com>
2024-05-23 18:50:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, render_template, send_file, redirect
 import requests
 import bs4
 import csv
+import os
 
 app = Flask(__name__)
 
@@ -104,4 +105,5 @@ def show_data():
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0",port=5000,debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
